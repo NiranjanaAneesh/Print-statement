01cbcd2037c9ccb8f74c3181841cc134f8ebacc4
NiranjanaAneesh <niranjanaaneeessh@gmail.com>
2025-04-03 18:53:19 +0530

    hello

---
+++ PrintStatement.py
@@ -0,0 +1,15 @@
+#Print a Sentence
+print("Welcome to the world of coding")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello coding \n")
+
+#Pass multiple arguments for printing
+print("hello",3)
+
+#End argument of Print Statement
+print("welcome to", end="codingal")
+
