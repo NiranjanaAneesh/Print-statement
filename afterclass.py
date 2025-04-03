8a71fd4f691301569d5c6c9c317a524be6787408
NiranjanaAneesh <niranjanaaneeessh@gmail.com>
2025-04-03 19:13:52 +0530

    heyy

---
+++ afterclass.py
@@ -0,0 +1,17 @@
+Niranjana = input("Enter your Birth date:")
+print("Niranjana's birhday is on",Niranjana)
+Gaya = input("Enter your Birth date:")
+print("Gaya's birhday is on",Gaya)
+Keerthana = input("Enter your Birth date:")
+print("Keerthana's birhday is on",Keerthana)
+Ananthalekshmi = input("Enter your Birth date:")
+print("Ananthalekshmi's birhday is on",Ananthalekshmi)
+Sariga = input("Enter your Birth date:")
+print("Sariga's birhday is on",Sariga)
+Dakshina = input("Enter your Birth date:")
+print("Dakshina's birhday is on",Dakshina)
+Mila = input("Enter your Birth date:")
+print("Mila's birhday is on",Mila)
+
+
+
