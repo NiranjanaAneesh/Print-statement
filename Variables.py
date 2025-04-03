11bd1925d7af9996d71a98a8aa2db6f48c188c75
NiranjanaAneesh <niranjanaaneeessh@gmail.com>
2025-04-03 19:00:46 +0530

    hi

---
+++ Variables.py
@@ -0,0 +1,13 @@
+#Creating different variables
+x = 3
+y = "San"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#User Input
+name = input("enter your name:")
+
+print("\nHello", name, "\nwelcome to codingal")
